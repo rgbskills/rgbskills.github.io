a35dd50a546737f2222598b85c0f3ddb3579efee
Zoltan Benko <rgbskills@gmail.com>
2017-02-16 15:44:09 +0200

    trakin.io landing

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,10 @@
-$('#index a').click(function(e){
-  e.preventDefault();
-});
-
-$('.resources').click(function(){
-  $('#resources').fadeIn();
-});
-$('.skills').click(function(){
-  $('#skills').fadeIn();
-});
-$('.about').click(function(){
-  $('#about').fadeIn();
-});
-
-$('.close-modal').click(function(){
-   $(this).parent().fadeOut();
-});
-
-function modal(){
-  var windowHeight = $(window).height();
-  $('.modal').css('min-height', windowHeight);
-}
-modal();
-
-$(window).resize(function(){
-  modal();
-});
+$(document).ready(function(){
+  var logo = function(){
+    var winHeight = $(window).height();
+    $('.logo-box').css('min-height', winHeight);
+  }
+  logo();
+  $(window).resize(function(){
+    logo();
+  });
+})
